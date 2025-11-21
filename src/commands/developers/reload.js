1465241a1f65859b9e092ff61d1db5978bf916be
Max <OllieBate2007@outlook.com>
2025-11-21 21:01:58 +0000

    test

---
+++ src/commands/developers/reload.js
@@ -0,0 +1,33 @@
+const {
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  ActivityType,
+  EmbedBuilder,
+  Client,
+  ChatInputCommandInteraction,
+} = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("reload")
+    .setDescription("Special command. If you know you know what its for")
+    .toJSON(),
+  userPermissions: [PermissionFlagsBits.SendMessages],
+  botPermissions: [PermissionFlagsBits.SendMessages],
+  devOnly: true,
+  /**
+   * @param { Client } client
+   * @param { ChatInputCommandInteraction } interaction
+   */
+  run: async (client, interaction) => {
+    await interaction.deferReply({ ephemeral: true });
+    try {
+        await axios.post("http://server.flatstudios.net/webhook/live")
+        await interaction.editReply("Reload signal sent successfully.");
+    } catch (error) {
+        console.error("Error sending reload signal:", error);
+        await interaction.editReply("Failed to send reload signal.");
+    }
+  },
+};
